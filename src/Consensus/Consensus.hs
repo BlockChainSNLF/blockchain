dfbec806abdd85a39a8c138e0050714676f74e86
Feyannibelli <fyann06@gmail.com>
2026-03-25 00:22:19 -0300

    feat: agregar validación de bloques, cadena y consenso (Parte 3 y 4)
    - BlockValidation: validación de hash, proof of work y encadenamiento
    - ChainValidation: validación de la cadena completa contra el génesis
    - Consensus: algoritmo de cadena válida más larga
    - TestValidation: suite de tests manuales sin depender del minero

---
+++ src/Consensus/Consensus.hs
@@ -0,0 +1,28 @@
+module Consensus.Consensus where
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+import Types.Block (Block)
+import Validation.ChainValidation (isValidChain)
+
+resolveChain :: [Block] -> [[Block]] -> [Block]
+resolveChain myChain peerChains =
+  let allChains = myChain : peerChains
+      validChains = filter isValidChain allChains
+   in case validChains of
+        [] -> myChain
+        _ -> maximumBy (comparing length) validChains
+
+shouldReplace :: [Block] -> [Block] -> Bool
+shouldReplace myChain candidate =
+  isValidChain candidate && length candidate > length myChain
+
+data ConsensusResult
+  = KeepMine
+  | AdoptPeer [Block]
+  deriving (Show)
+
+consensusWithOne :: [Block] -> [Block] -> ConsensusResult
+consensusWithOne myChain peerChain
+  | shouldReplace myChain peerChain = AdoptPeer peerChain
+  | otherwise = KeepMine
---
+++ src/Validations/ChainValidation.hs
@@ -0,0 +1,43 @@
+module Validation.BlockValidation where
+
+import Hashing.Hash (hashString)
+import Hashing.Serialization (serializePreBlock)
+import Types.Block (Block (..))
+import Types.PreBlock (PreBlock (..))
+
+difficulty :: Int
+difficulty = 3
+
+requiredPrefix :: String
+requiredPrefix = replicate difficulty '0'
+
+hasValidHash :: Block -> Bool
+hasValidHash block =
+  hashValue block == hashString (serializePreBlock (blockContent block))
+
+meetsProofOfWork :: Block -> Bool
+meetsProofOfWork block =
+  take difficulty (hashValue block) == requiredPrefix
+
+linksTo :: Block -> Block -> Bool
+linksTo prevBlock nextBlock =
+  previousHash (blockContent nextBlock) == hashValue prevBlock
+    && index (blockContent nextBlock) == index (blockContent prevBlock) + 1
+
+isValidBlock :: Block -> Block -> Bool
+isValidBlock prevBlock newBlock =
+  hasValidHash newBlock
+    && meetsProofOfWork newBlock
+    && linksTo prevBlock newBlock
+
+data BlockError
+  = InvalidHash
+  | FailedPoW
+  | BrokenLink
+  deriving (Show, Eq)
+
+validateBlockDetailed :: Block -> Block -> [BlockError]
+validateBlockDetailed prevBlock newBlock =
+  [InvalidHash | not (hasValidHash newBlock)]
+    ++ [FailedPoW | not (meetsProofOfWork newBlock)]
+    ++ [BrokenLink | not (linksTo prevBlock newBlock)]
