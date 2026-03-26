71a7de510251d604fe86d230d147d0fafb8c0a02
ElChurro274 <santinocolombo13@gmail.com>
2026-03-26 19:45:25 -0300

    :

--- src/Hashing/Mining.hs
+++ src/Hashing/Mining.hs
@@ -1,14 +1,9 @@
 module Hashing.Mining where
 
-import Control.Concurrent.STM
 import Hashing.Hash (hashString)
 import Hashing.Serialization (serializePreBlock)
 import Types.Block (Block, blockContent, createBlock, hashValue)
-import Types.PreBlock (PreBlock (..), nonce)
-import Types.Transaction (Transaction)
-
-blockSize :: Int
-blockSize = 5
+import Types.PreBlock (PreBlock, nonce)
 
 hashPreBlock :: PreBlock -> String
 hashPreBlock = hashString . serializePreBlock
@@ -19,39 +14,14 @@ isHashValidForDifficulty difficulty hashResult =
   where
     normalizedDifficulty = max 0 difficulty
 
-buildPreBlock :: Block -> [Transaction] -> PreBlock
-buildPreBlock lastBlock txs =
-  PreBlock
-    { index        = index (blockContent lastBlock) + 1
-    , timestamp    = timestamp (blockContent lastBlock) + 1
-    , transactions = take blockSize txs
-    , previousHash = hashValue lastBlock
-    , nonce        = 0
-    }
-
 mineBlock :: Int -> PreBlock -> Block
 mineBlock difficulty = go . applyNonce 0
   where
     go candidate
-      | isHashValidForDifficulty difficulty candidateHash =
-          createBlock candidate candidateHash
-      | otherwise =
-          go (applyNonce (nonce candidate + 1) candidate)
+      | isHashValidForDifficulty difficulty candidateHash = createBlock candidate
+      | otherwise = go (applyNonce (nonce candidate + 1) candidate)
       where
         candidateHash = hashPreBlock candidate
-
-mineBlockInterruptible :: Int -> PreBlock -> TVar Bool -> IO (Maybe Block)
-mineBlockInterruptible difficulty pb interrupt = go (applyNonce 0 pb)
-  where
-    go candidate = do
-      shouldStop <- readTVarIO interrupt
-      if shouldStop
-        then return Nothing
-        else do
-          let h = hashPreBlock candidate
-          if isHashValidForDifficulty difficulty h
-            then return (Just (createBlock candidate h))
-            else go (applyNonce (nonce candidate + 1) candidate)
 
 applyNonce :: Int -> PreBlock -> PreBlock
 applyNonce value preBlock = preBlock {nonce = value}
@@ -60,5 +30,5 @@ isBlockMinedCorrectly :: Int -> Block -> Bool
 isBlockMinedCorrectly difficulty block =
   storedHash == recomputedHash && isHashValidForDifficulty difficulty storedHash
   where
-    storedHash     = hashValue block
+    storedHash = hashValue block
     recomputedHash = hashPreBlock (blockContent block)
--- src/Validations/BlockValidation.hs
+++ src/Validations/BlockValidation.hs
@@ -1,15 +1,44 @@
 module Validations.BlockValidation where
 
-import Hashing.Mining (isBlockMinedCorrectly)
+import Hashing.Hash (hashString)
+import Hashing.Serialization (serializePreBlock)
 import Types.Block (Block (..))
 import Types.PreBlock (PreBlock (..))
+
+-- Coordinar con Nico (Parte 2). Debe ser igual al valor usado al minar.
+difficulty :: Int
+difficulty = 3
+
+requiredPrefix :: String
+requiredPrefix = replicate difficulty '0'
+
+hasValidHash :: Block -> Bool
+hasValidHash block =
+  hashValue block == hashString (serializePreBlock (blockContent block))
+
+meetsProofOfWork :: Block -> Bool
+meetsProofOfWork block =
+  take difficulty (hashValue block) == requiredPrefix
 
 linksTo :: Block -> Block -> Bool
 linksTo prevBlock nextBlock =
   previousHash (blockContent nextBlock) == hashValue prevBlock
     && index (blockContent nextBlock) == index (blockContent prevBlock) + 1
 
-isValidBlock :: Int -> Block -> Block -> Bool
-isValidBlock difficulty prevBlock newBlock =
-  isBlockMinedCorrectly difficulty newBlock
+isValidBlock :: Block -> Block -> Bool
+isValidBlock prevBlock newBlock =
+  hasValidHash newBlock
+    && meetsProofOfWork newBlock
     && linksTo prevBlock newBlock
+
+data BlockError
+  = InvalidHash
+  | FailedPoW
+  | BrokenLink
+  deriving (Show, Eq)
+
+validateBlockDetailed :: Block -> Block -> [BlockError]
+validateBlockDetailed prevBlock newBlock =
+  [InvalidHash | not (hasValidHash newBlock)]
+    ++ [FailedPoW | not (meetsProofOfWork newBlock)]
+    ++ [BrokenLink | not (linksTo prevBlock newBlock)]
